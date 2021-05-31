0d5a6a81263b00e6408ed80efddbb794bfe677ac
Brian Cartan <cartanb@gmail.com>
2021-05-31 17:40:47 -0400

    Finished '3. Redux'

--- client/index.js
+++ client/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux';
 
-ReactDOM.render(<div>hello world!</div>, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <div>hello world!</div>
+  </Provider>,
+  document.getElementById('app')
+);
